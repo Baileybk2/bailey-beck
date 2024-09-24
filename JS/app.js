feaf78ad7e395863f35f94eae23835c764a276a1
BaileyBeck <baileybk2@gmail.com>
2024-09-24 15:28:55 -0400

    added background colors, cleaned up code

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const pillSection = document.getElementById("technical")
+let pills = document.getElementsByClassName("pill-animate")
 
-  let triggerPillAnimation = () => {
-    let pills = document.getElementsByClassName("pill-animate")
-
-    Array.from(pills).forEach((pill, index) => {
-      pill.style.animationDelay = `${index * 0.1}s`
-      pill.style.opacity = 1
-    })
-  }
-  const observer = new IntersectionObserver(
-    (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          triggerPillAnimation()
-          observer.unobserve(pillSection)
-        }
-      })
-    },
-    { threshold: 0 }
-  )
-
-  observer.observe(pillSection)
+Array.from(pills).forEach((pill, index) => {
+  pill.style.animationDelay = `${index * 0.15}s`
 })
